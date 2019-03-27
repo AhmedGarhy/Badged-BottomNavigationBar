80e2492be68267511ecfe40badb75ddf77c595c5
Ahmed Ewess <ahmed.garhy@vodafone.com>
2019-03-27 14:55:03 +0200

    first commit - adding simple badge dot on the navigation bottom items

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.badgedbottomnavigationbar.BadgedBottomNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private BadgedBottomNavigationBar navigation;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    navigation.getMenu().getItem(0).setIcon(R.drawable.ic_home_black_24dp);
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+//    public void updateNavigationBarBadges(String[] ids) {
+//        for (String screenId : ids) {
+//            int menuItemIndex = 2;
+//            for (int i = 0; i < 2; i++) {
+//                if (navigation.getMenu().getItem(i).getTitle().equals(screenId)) {
+//                    menuItemIndex = i;
+//                    break;
+//                }
+//            }
+//            navigation.showBadgeOnMenuItem(menuItemIndex);
+//        }
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        navigation = (BadgedBottomNavigationBar) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 3; i++) {
+                    navigation.showBadgeOnMenuItem(i);
+                }
+            }
+        });
+
+        findViewById(R.id.remove_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 3; i++) {
+                    navigation.removeBadge(i);
+                }
+            }
+        });
+
+    }
+}
---
+++ badgedbottomnavigationbar/src/main/java/com/badgedbottomnavigationbar/BadgedBottomNavigationBar.java
@@ -0,0 +1,68 @@
+package com.badgedbottomnavigationbar;
+import android.content.Context;
+import android.support.design.widget.BottomNavigationView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BadgedBottomNavigationBar extends  BottomNavigationView{
+
+    public BadgedBottomNavigationBar(Context context) {
+        super(context);
+    }
+
+    public BadgedBottomNavigationBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BadgedBottomNavigationBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    /**
+     * show the badge on the menu item view.
+     *
+     * @param menuItemIndex
+     */
+    public void showBadgeOnMenuItem(int menuItemIndex) {
+        android.support.design.internal.BottomNavigationMenuView bottomNavigationView =
+                (android.support.design.internal.BottomNavigationMenuView) getChildAt(0);
+        View view = bottomNavigationView.getChildAt(menuItemIndex);
+        if (view instanceof ViewGroup) {
+            //NUMBER_OF_MENU_ITEM_VIEW_CHILDERN_WITHOUT_BADGE
+            if (((ViewGroup) view).getChildCount() > 2) {
+                return;
+            }
+        }
+        android.support.design.internal.BottomNavigationItemView bottomNavigationItemView =
+                (android.support.design.internal.BottomNavigationItemView) view;
+
+        LayoutInflater.from(getContext()).inflate(R.layout.notification_badge, bottomNavigationItemView,
+                true);
+
+    }
+
+    /**
+     * this method to remove dot [badge view] if it's already inflated on the menu item.
+     *
+     * @param menuItemIndex  the menu item index
+     */
+    public void removeBadge(int menuItemIndex) {
+        android.support.design.internal.BottomNavigationMenuView bottomNavigationMenuView =
+                (android.support.design.internal.BottomNavigationMenuView) getChildAt(0);
+        View v = bottomNavigationMenuView.getChildAt(menuItemIndex);
+        // check if the badge is already displayed on the icon.
+        if (v instanceof ViewGroup) {
+            int childCount = ((ViewGroup) v).getChildCount();
+            /* this condition to prevent the inflating the badge more than one time on the
+             menu item .. because this means that the badge is already inflated before*/
+            // 3 is the NUMBER_OF_MENU_ITEM_VIEW_CHILDERN_WITH_BADGE
+            if (childCount < 3) return;
+        }
+        android.support.design.internal.BottomNavigationItemView itemView = (android.support.design.internal.BottomNavigationItemView) v;
+        // remove the last child [badge view]
+        itemView.removeViewAt(itemView.getChildCount() - 1);
+    }
+
+}
